e4626fca3eba08ee544df58ce1d5acc74bd0bb65
Hemant Rathore <hemant0hack@gmail.com>
2026-08-29 22:35:11 +0530

    Day05

--- Contitional/largestNo.java
+++ Contitional/largestNo.java
@@ -5,16 +5,34 @@ public class largestNo{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
         
+        // System.out.println("Enter first no.:");
+        // int a = input.nextInt();
+
+        // System.out.println("Enter second no.:");
+        // int b = input.nextInt();
+
+        // if(a>b){
+        //     System.out.println("Largest First no.");
+        // }else{
+        //     System.out.println("Largest second no.");
+        // }
+
         System.out.println("Enter first no.:");
-        int a = input.nextInt();
+        int x = input.nextInt();
 
         System.out.println("Enter second no.:");
-        int b = input.nextInt();
+        int y = input.nextInt();
 
-        if(a>b){
-            System.out.println("Largest First no.");
-        }else{
-            System.out.println("Largest second no.");
+        System.out.println("Enter third no.:");
+        int z = input.nextInt();
+
+        if(x>y && x>z){
+            System.out.println("Largest First no."+x);
+        }else if(y>x && y>z){
+            System.out.println("Largest second no."+y);
+        }
+        else {
+            System.out.println("Largest Third no."+z);
         }
     }
 }
