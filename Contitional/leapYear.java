ce1dfefc5df5eba24b8cf99c9af4a3b30ebd5c63
Hemant Rathore <hemant0hack@gmail.com>
2026-08-30 21:01:59 +0530

    Day06

---
+++ Contitional/leapYear.java
@@ -0,0 +1,12 @@
+
+import java.util.Scanner;
+
+public class leapYear{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter year:");
+        int year = input.nextInt();
+        ,
+    }
+}
