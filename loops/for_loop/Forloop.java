e51de81b98eaf625ef9c49fa54a891afe749b0ba
Hemant Rathore <hemant0hack@gmail.com>
2026-09-01 23:46:22 +0530

    Day07

--- loops/for_loop/Forloop.java
+++ loops/for_loop/Forloop.java
@@ -4,28 +4,28 @@ public class Forloop{
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // System.out.print("Enter your num :");
-        // int n = input.nextInt();
+        System.out.print("Enter your num :");
+        int n = input.nextInt();
 
-        // for (int i = 1; i <= n; i++){
-        //     System.out.println(i);   
-        // }
+        for (int i = 1; i <= n; i++){
+            System.out.println(i);   
+        }
 
-        // System.out.print("Enter your table num :");
-        // int t = input.nextInt();  
+        System.out.print("Enter your table num :");
+        int t = input.nextInt();  
 
-        // for (int x=1; x<=10; x++){
-        //     int table = t*x;
-        //     System.out.println(t+"x"+x+"="+table);
-        // }
+        for (int x=1; x<=10; x++){
+            int table = t*x;
+            System.out.println(t+"x"+x+"="+table);
+        }
 
-        // System.out.print("Enter your num :");
-        // int e = input.nextInt();  
+        System.out.print("Enter your num :");
+        int e = input.nextInt();  
 
-        // for (int x=1; x<=e; x++){
-        //     if (x%2==0)
-        //     System.out.println(x);
-        // }
+        for (int x=1; x<=e; x++){
+            if (x%2==0)
+            System.out.println(x);
+        }
 
         int num = input.nextInt();
         int rev = 0;
