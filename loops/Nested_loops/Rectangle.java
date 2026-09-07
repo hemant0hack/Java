2061810f71d8f7be4ff51075fd3c52113e7fc798
Hemant Rathore <hemant0hack@gmail.com>
2026-09-07 23:37:15 +0530

    Day10

--- loops/Nested_loops/Rectangle.java
+++ loops/Nested_loops/Rectangle.java
@@ -1,11 +1,11 @@
-public class Rectangle{
+public class rectangle{
     public static void main(String[] args) {
         
         for (int i = 1; i <= 3; i++) {
             for( int j = 1; j <= 6; j++){
-                System.out.println("*");
+                System.out.print("*");
             }
-            System.err.println();
+            System.out.println();
         }
     }
 }
