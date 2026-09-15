d91ba5b6736a1833dcbf8fd166648cd73b4d0135
Hemant Rathore <hemant0hack@gmail.com>
2026-09-16 00:36:29 +0530

    day20

---
+++ Arrays/cars.java
@@ -0,0 +1,24 @@
+class cars{
+    public static void main(String[] args) {
+    String[] cars = {"Volvo", "BMW", "Ford","Mazda"};
+
+    // Access the Elemnts of an Array.
+    System.out.println(cars[0]);
+    
+    //Change an Array Elemnts.
+    cars[0] = "Opel" ;
+    System.out.println(cars[0]);
+
+    // Array length
+    System.out.println(cars.length);
+        
+    // Using new Keyword
+    String[] car = new String[4];        // size is 4
+    car[0] = "Volvo";
+    car[1] = "BMW";
+    car[2] = "Ford";
+    car[3] = "Mazda";
+
+    System.out.println(car[0]);
+    }
+}
---
+++ Arrays/readme.md
@@ -0,0 +1,14 @@
+# Java Arrays
+
+### Arrays are used to stroe multiple value in a single variable, instead of declaring separate variables for each value. 
+
+### To declare an array, define the variable type with **square brackets**[]:
+
+### Access the Elements of an Array
+
+### Change an Array Elements
+
+### Array Length
+
+### The **new** Keyword
+#### You can also create an array y specifying its size with **new** This make an empty array with space for  a fixednumber of elements, which you can fill later:
