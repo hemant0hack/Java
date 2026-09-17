bea90cdd2db37efeb44651ea215a343959c6cf90
Hemant Rathore <hemant0hack@gmail.com>
2026-09-17 11:59:23 +0530

    Day22

---
+++ Arrays/marks.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+
+class marks{
+    static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the Length of Array : ");
+        int n = input.nextInt();
+
+        int marks[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter number of index no. "+ i + ":" );
+            marks[i] = input.nextInt();
+        }
+        
+        for (int i = 0; i < marks.length; i++) {
+            System.out.println(marks[i]);
+        }
+    }
+}
